7411208e4c54b7126f6db38c7ade0cdc79431c12
Ken Stanley <dohpaz@gmail.com>
2022-11-28 20:37:25 -0500

    Make it Work(TM)

--- app/src/main/java/com/example/group22_hw10/MainActivity.java
+++ app/src/main/java/com/example/group22_hw10/MainActivity.java
@@ -17,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rootView, new LoginFragment())
+                .commit();
     }
 }
