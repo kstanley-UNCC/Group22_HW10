81e54d811a4d19a5862c1fe014a7839fc694b331
Ken Stanley <dohpaz@gmail.com>
2022-11-28 20:42:50 -0500

    Set up the remaining fragments

--- app/src/main/java/com/example/group22_hw10/CreateAccountFragment.java
+++ app/src/main/java/com/example/group22_hw10/CreateAccountFragment.java
@@ -6,11 +6,16 @@ package com.example.group22_hw10;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.group22_hw10.databinding.FragmentCreateAccountBinding;
+import com.example.group22_hw10.databinding.FragmentCreateTripBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +23,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CreateAccountFragment extends Fragment {
+    FragmentCreateAccountBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,9 +66,16 @@ public class CreateAccountFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false);
+        binding = FragmentCreateAccountBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        requireActivity().setTitle(R.string.create_account_title);
     }
 }
--- app/src/main/java/com/example/group22_hw10/TripDetailsFragment.java
+++ app/src/main/java/com/example/group22_hw10/TripDetailsFragment.java
@@ -6,11 +6,15 @@ package com.example.group22_hw10;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.group22_hw10.databinding.FragmentTripDetailsBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +22,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class TripDetailsFragment extends Fragment {
+    FragmentTripDetailsBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,9 +65,16 @@ public class TripDetailsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip_details, container, false);
+        binding = FragmentTripDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        requireActivity().setTitle(R.string.trip_details_title);
     }
 }
