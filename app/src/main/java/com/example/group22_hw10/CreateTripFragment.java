8904e9d5932a523e84428e263d75669976d4eb10
Ken Stanley <dohpaz@gmail.com>
2022-12-02 16:20:22 -0500

    Remove unused imports

--- app/src/main/java/com/example/group22_hw10/CreateTripFragment.java
+++ app/src/main/java/com/example/group22_hw10/CreateTripFragment.java
@@ -24,10 +24,7 @@ import com.example.group22_hw10.databinding.FragmentCreateTripBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.Priority;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 public class CreateTripFragment extends Fragment {
 
