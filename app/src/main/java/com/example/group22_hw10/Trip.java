6d844fb17c3c1b174d7c95e0564b8ed9f5d4f1d2
Ken Stanley <dohpaz@gmail.com>
2022-12-04 08:48:22 -0500

    Add full constructor to initialize all properties

--- app/src/main/java/com/example/group22_hw10/Trip.java
+++ app/src/main/java/com/example/group22_hw10/Trip.java
@@ -24,6 +24,20 @@ public class Trip implements Serializable {
     public Trip() {
     }
 
+    public Trip(String trip_id, String user_id, String trip_name, Timestamp created_at, Timestamp completed_at, double start_latitude, double start_longitude, double end_latitude, double end_longitude, double total_miles, String status) {
+        this.trip_id = trip_id;
+        this.user_id = user_id;
+        this.trip_name = trip_name;
+        this.created_at = created_at;
+        this.completed_at = completed_at;
+        this.start_latitude = start_latitude;
+        this.start_longitude = start_longitude;
+        this.end_latitude = end_latitude;
+        this.end_longitude = end_longitude;
+        this.total_miles = total_miles;
+        this.status = status;
+    }
+
     public Trip(String user_id, String trip_name, double start_latitude, double start_longitude) {
         this.trip_id = UUID.randomUUID().toString();
         this.user_id = user_id;
